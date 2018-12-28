62ae52ea697e1afdbb9345356adaa42bd701cae9
Jimmy <wujimmy94@gmail.com>
2018-12-28 16:48:55 -0500

    Returns image labels using Google Vision Api

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,23 @@ class Image < ApplicationRecord
 
   validate :attachment_or_url_present
 
+  def analyze_and_return_labels_with_score
+    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    file_name = self.image_on_disk
+    response = image_annotator.label_detection image: file_name
+    return_data = Hash.new()
+    response.responses.each do |res|
+      res.label_annotations.each do |label|
+        return_data[label.description] = label.score
+      end
+    end
+    return_data
+  end
+
+  def image_on_disk
+    ActiveStorage::Blob.service.send(:path_for, self.attachment.key)
+  end
+
   private
 
   def attachment_or_url_present
