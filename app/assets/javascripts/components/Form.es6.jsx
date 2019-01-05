f2cc1e6c1ac0840087052910feadabb8f6fb990f
Jimmy <wujimmy94@gmail.com>
2019-01-05 14:14:14 -0500

    imported classless css

--- app/assets/javascripts/components/Form.es6.jsx
+++ app/assets/javascripts/components/Form.es6.jsx
@@ -11,16 +11,30 @@ class Form extends React.Component {
     }
   }
 
-  componentDidMount() {
-    if (this.state.dropzone) initDropzone()
+  componentWillMount() {
+    if (this.state.dropzone) this.initDropzone.bind(this)
+  }
+
+  initDropzone() {
+    Dropzone.options.appDropzone = {
+      paramName: "file", // The name that will be used to transfer the file
+      maxFilesize: 10, // MB
+      maxFiles: 1,
+      acceptedFiles: "image/*",
+      success: function(file, res) {
+
+      }
+    }
   }
 
   render() {
     const {url, type, data, dropzone, authenticityToken} = this.state
     return (
-      <form action={url} className={dropzone ? "dropzone dz-clickable" : ""} id={dropzone ? "app_dropzone" : ""} method={type}>
-        <input type="hidden" name="authenticity_token" value={authenticityToken} />
-      </form>
+      <div className="row">
+        <form action={url} className={dropzone ? "dropzone dz-clickable" : ""} id={dropzone ? "app_dropzone" : ""} method={type}>
+          <input type="hidden" name="authenticity_token" value={authenticityToken} />
+        </form>
+      </div>
     )
   }
 }
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,8 @@
-initDropzone = function() {
-  Dropzone.options.appDropzone = {
-    paramName: "file", // The name that will be used to transfer the file
-    maxFilesize: 10, // MB
-    maxFiles: 1,
-    acceptedFiles: "image/*"
-  }
-}
+// initDropzone = function() {
+//   Dropzone.options.appDropzone = {
+//     paramName: "file", // The name that will be used to transfer the file
+//     maxFilesize: 10, // MB
+//     maxFiles: 1,
+//     acceptedFiles: "image/*"
+//   }
+// }
